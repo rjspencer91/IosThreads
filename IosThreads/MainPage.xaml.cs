5d0f56943412b94c7c42c0592ce7b3d2666b03dc
Robert Spencer <robert.spencer@amiosec.com>
2024-07-31 17:38:02 +0100

    First

---
+++ IosThreads/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace IosThreads;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		await GetAsync("https://www.example.com");
+	}
+
+	public async Task<string> GetAsync(string url)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return null;
+            }
+        }
+    }
+}
+
